416eda3275549bdacd7a1da4d5da9911ae21447e
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-04-13 22:54:09 +1100

    v1

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+public class Post {
+    private int postId;
+    private int postTime;
+    private String AuthorWidget;
+    private String menuPic;
+    private String postBody;
+    private Boolean postBodyCut;
+    private String imagePreview;
+    private String imageLink;
+    private String attachmentWidget;
+    private String videoWidget;
+    private String linkWidget;
+    private String likeWidget;
+    private String commentWidget;
+    private String shareWidget;
+    private String viewWidget;
+    private String topCommentsWidget;
+
+}
+
+
